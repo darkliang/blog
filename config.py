220aaf66fb3e83ce6756c6f435b21d62dafe6d1e
Junhao Liang <43094337+darkliang@users.noreply.github.com>
2021-02-22 19:13:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ index_page_size = 10
 archives_page_size = 30
 fetch_remote_imgs = False
 enable_jsdelivr = {
-    "enabled": False,
+    "enabled": True,
     "repo": "darkliang/blog@master"
 }
 locale = "Asia/Shanghai"
