84c1a2b088317f976ee5f38939878eecf111b01c
darkliang <11710911@mail.sustech.edu.cn>
2021-02-22 18:34:49 +0800

    change config

--- config.py
+++ config.py
@@ -4,41 +4,32 @@
 
 # For Maverick
 site_prefix = "/"
-source_dir = "./test_src/"
-build_dir = "./test_dist/"
+source_dir = "./src/"
+build_dir = "./dist/"
 template = "Galileo"
 index_page_size = 10
 archives_page_size = 30
 fetch_remote_imgs = False
 enable_jsdelivr = {
     "enabled": True,
-    "repo": "AlanDecode/Maverick@gh-pages"
+    "repo": "darkliang/darkliang.github.io@gh-pages"
 }
 locale = "Asia/Shanghai"
 category_by_folder = False
 
 # For site
-site_name = "Maverick"
+site_name = "Leeeung"
 site_logo = "${static_prefix}android-chrome-512x512.png"
 site_build_date = "2019-12-06T12:00+08:00"
 author = "AlanDecode"
 email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
+author_homepage = "https://www.leeeung.com"
 description = "This is Maverick, Theme Galileo."
 key_words = ["Maverick", "AlanDecode", "Galileo", "blog"]
 language = 'english'
 background_img = '${static_prefix}bg/The_Great_Wave_off_Kanagawa.jpg'
 external_links = [
-    {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
-    },
-    {
-        "name": "Triple NULL",
-        "url": "https://www.imalan.cn",
-        "brief": "Home page for AlanDecode."
-    }
+    
 ]
 nav = [
     {
@@ -60,19 +51,9 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/darkliang",
         "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
     }
 ]
 
